e71be12a29bbdcb35bfe939860723f5f2304f937
Druja David <davidddruja10@gmail.com>
2025-07-18 16:31:04 +0300

    Added cross-platform support (Added Windows support)

--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++ cmake-build-debug/.qt/QtDeployTargets.cmake
@@ -1,2 +1,3 @@
-set(__QT_DEPLOY_TARGET_TaskManagement_FILE /Users/daviddruja/Documents/Facultate/OOP/Project/TaskManagement/cmake-build-debug/TaskManagement)
+set(__QT_DEPLOY_TARGET_TaskManagement_FILE D:/projects/Programe/TaskManagementApp/TaskManagementApp/cmake-build-debug/TaskManagement.exe)
 set(__QT_DEPLOY_TARGET_TaskManagement_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_TaskManagement_RUNTIME_DLLS D:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
--- cmake-build-debug/TaskManagement_autogen/UYX5XTB5RZ/moc_mainwindow.cpp
+++ cmake-build-debug/TaskManagement_autogen/UYX5XTB5RZ/moc_mainwindow.cpp
@@ -1,12 +1,12 @@
 /****************************************************************************
-** Meta object code from reading C++ file 'mainwindow.h'
+** Meta object code from reading C++ file 'MainWindow.h'
 **
-** Created by: The Qt Meta Object Compiler version 69 (Qt 6.9.0)
+** Created by: The Qt Meta Object Compiler version 69 (Qt 6.9.1)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../../ui/mainwindow.h"
+#include "../../../ui/MainWindow.h"
 #include <QtGui/qtextcursor.h>
 #include <QtCore/qmetatype.h>
 
@@ -17,9 +17,9 @@
 
 #include <QtCore/qxptype_traits.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
-#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#error "The header file 'MainWindow.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 69
-#error "This file was generated using the moc from 6.9.0. It"
+#error "This file was generated using the moc from 6.9.1. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
